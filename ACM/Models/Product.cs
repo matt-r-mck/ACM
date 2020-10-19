2524dacfa8d2e50d15057a16ea2654e30b250344
matt-r-mck <matt.r.mck@gmail.com>
2020-10-19 14:54:49 -0400

    Add project files.

---
+++ ACM/Models/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.Models
+{
+   class Product
+   {
+      public string Name { get; set; }
+      public string Description { get; set; }
+      public double CurrentPrice { get; set; }
+
+      public bool Validate()
+      {
+         var isValid = true;
+
+         if (string.IsNullOrWhiteSpace(Name)) isValid = false;
+         if (string.IsNullOrWhiteSpace(Description)) isValid = false;
+
+         return isValid;
+      }
+
+      public Product Retrieve(int customerId)
+      {
+         return new Product();
+      }
+
+      public List<Product> Retrieve()
+      {
+         return new List<Product>();
+      }
+
+      public bool Save()
+      {
+         return true;
+      }
+
+
+   }
+}
